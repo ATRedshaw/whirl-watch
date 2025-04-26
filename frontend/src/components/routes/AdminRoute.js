02f5ccb1eee7d8702d529ce90a98c94c41c3ce7b
ATRedshaw <redshawalex02@outlook.com>
2025-04-26 18:35:47 +0100

    Removed redundant import

--- frontend/src/components/routes/AdminRoute.js
+++ frontend/src/components/routes/AdminRoute.js
@@ -1,10 +1,8 @@
-import { useLocation } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import NotFound from '../NotFound';
 
 const AdminRoute = ({ children }) => {
   const { user, isAuthenticated, loading } = useAuth();
-  const location = useLocation();
 
   if (loading) {
     return <div>Loading...</div>;
